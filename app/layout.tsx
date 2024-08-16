f8ba705781e05254f36e56cf76911df2960b11fd
Sambhav Rana <sambhavrana27@gmail.com>
2024-08-16 18:15:09 +0530

    feat: Build navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from './NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'TrackMate - Issue Tracker',
-  description: 'TrackMate is an advanced issue tracker for all of your requirements',
+  title: 'TrackBuddy - Issue Tracker',
+  description: 'TrackBuddy is an advanced issue tracker for all of your requirements',
 }
 
 export default function RootLayout({
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        <main>
+        {children}
+        </main>
+        </body>
     </html>
   )
 }
