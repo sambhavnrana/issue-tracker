1717cfac52aad71ca0c2a23e4d49f02c99f08b31
Sambhav Rana <sambhavrana27@gmail.com>
2024-08-19 10:52:15 +0530

    UI : Added edit btn

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import prisma from '@/prisma/client'
 import { notFound } from 'next/navigation';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import ReactMarkdown from 'react-markdown';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 interface Props {
     params: { id: string }
@@ -22,17 +24,27 @@ const IssuesDetailPage = async ({ params }: Props) => {
 
 
     return (
-        <div>
-            <Heading>{issue.title}</Heading>
-            <Flex className='gap-3' my="2" >
-                <IssueStatusBadge status={issue.status} />
-                <Text>{issue.createdAt.toDateString()}</Text>
-            </Flex>
-            <Card className='prose' mt="4">
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
+        <Grid columns={{ initial: "1", md: "2" }} gap="5">
+            <Box>
 
-            </Card>
-        </div>
+                <Heading>{issue.title}</Heading>
+                <Flex className='gap-3' my="2" >
+                    <IssueStatusBadge status={issue.status} />
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                </Flex>
+                <Card className='prose' mt="4">
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${issue.id}/edit`}> </Link>
+                    {/* didn't use our customised Link as we want simple client side navigation here */}
+                    Edit Issue</Button>
+            </Box>
+        </Grid>
     )
 }
 
