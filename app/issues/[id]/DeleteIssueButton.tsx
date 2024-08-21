8aa0a62f97943abe48a3f4c47c019977ab7865f1
Sambhav Rana <sambhavrana27@gmail.com>
2024-08-21 17:16:24 +0530

    Revert "feat: Delete an issue"
    This reverts commit 59b7d38460c6fe0ab1c96f5d2bc7e57fe5578215.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,20 +1,13 @@
 "use client";
 
 import { AlertDialog, Button, Flex } from "@radix-ui/themes"
-import axios from "axios";
-import { useRouter } from "next/navigation";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-    const router = useRouter();
     return (
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color="red" onClick={async () => {
-                        await axios.delete('/api/issues/' + issueId);
-                        router.push('/issues');
-                        router.refresh();
-                    }}>Delete Issue</Button>
+                    <Button color="red">Delete Issue</Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content>
                     <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
