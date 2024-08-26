0bd7f17b3ef1953b4162a17063ab3198bb5448c6
Sambhav Rana <sambhavrana27@gmail.com>
2024-08-27 00:41:16 +0530

    fix: sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -21,13 +21,14 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ]
   const statuses = Object.values(Status)
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' } : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status
     },
-    orderBy: {
-      createdAt: 'desc'
-    }
+
   });
   return (
     <div>
