bbdbd883027beea95ab0d3ad0fcb7e3183f5514b
Sambhav Rana <sambhavrana27@gmail.com>
2024-08-26 23:05:00 +0530

    fix: enhanced security

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { auth } from "./auth";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+  }
 }
 
 export const config = {
