382b89f1546204765ed8fd4baed9f3f702d0a6b7
Sambhav Rana <89438969+sambhavnrana@users.noreply.github.com>
2026-05-16 00:21:08 +0530

    Update footer link to new personal website

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="w-full px-3 sm:px-4 md:px-6 py-2 sm:py-3 text-center text-xs sm:text-sm md:text-base lg:bg-gray-100 bg-gray-200 text-slate-600 shadow animate-fade-in [animation-delay:0.5s]">
             &copy; {new Date().getFullYear()} <Link href="https://www.github.com/sambhavnrana/issue-tracker" target="_blank"
                 rel="noopener noreferrer" className="underline text-brand-dark"> TrackBuddy</Link>. Developed by{' '}
-            <Link href="https://www.sambhavrana.me" target="_blank"
+            <Link href="https://www.sambhavrana.dev" target="_blank"
                 rel="noopener noreferrer" className="underline text-brand-dark">Sambhav Rana</Link>
         </footer>
     )
